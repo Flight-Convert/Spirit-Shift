aa634226af62cb3de801f740ad0c36db865744e3
Anthony <awessel@mail.bradley.edu>
2020-09-11 18:06:55 -0500

    Added initial body switcher script
    currently hides enemies based on layer
    TODO: add functionality to swap which enemy is being controlled by player

---
+++ Spirit Shift/Assets/BodySwitcher.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Anthony Wessel
+ * Project 1 (Spririt Shift)
+ * 
+ * Swaps player control to different enemies and hides enemies
+ * based on which team player controlled enemy is on
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodySwitcher : MonoBehaviour
+{
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B)) showBlueTeam();
+        if (Input.GetKeyDown(KeyCode.R)) showRedTeam();
+        if (Input.GetKeyDown(KeyCode.G)) showBothTeams();
+    }
+
+    #region helper methods
+
+    /// Methods to choose which team(s) to show
+    void showBlueTeam()
+    {
+        showLayer("BlueTeam");
+        hideLayer("RedTeam");
+    }
+
+    void showRedTeam()
+    {
+        hideLayer("BlueTeam");
+        showLayer("RedTeam");
+    }
+
+    void showBothTeams()
+    {
+        showLayer("BlueTeam");
+        showLayer("RedTeam");
+    }
+
+    void hideBothTeams()
+    {
+        hideLayer("BlueTeam");
+        hideLayer("RedTeam");
+    }
+
+    /// Methods to show/hide specific layers
+    void showLayer(int layer)
+    {
+        cam.cullingMask |= 1 << layer;
+    }
+
+    void showLayer(string layer)
+    {
+        showLayer(LayerMask.NameToLayer(layer));
+    }
+
+    void hideLayer(int layer)
+    {
+        cam.cullingMask &= ~(1 << layer);
+    }
+
+    void hideLayer(string layer)
+    {
+        hideLayer(LayerMask.NameToLayer(layer));
+    }
+    #endregion
+}
