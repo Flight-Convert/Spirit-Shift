28261cd3cedf35303fdb14d4d5723eb083ce1b23
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2020-09-12 19:11:09 -0500

    Enabled enemies to attack

---
+++ Spirit Shift/Assets/Scripts/attackPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackPlayer : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private GameObject player;
+    private float distance;
+    public float threshold;
+    public float delay;
+    public GameObject fist;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb2d = GetComponent<Rigidbody2D>();
+        fist.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(rb2d.transform.position, player.transform.position);
+
+        if (distance <= threshold)
+        {
+            StartCoroutine(punch());
+        }
+    }
+
+    IEnumerator punch()
+    {
+        fist.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        fist.SetActive(false);
+        yield return true;
+    }
+}
