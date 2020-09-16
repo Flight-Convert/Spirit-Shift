dad22c945b77434cae4c17504e5f34ad2a9d05ec
Anthony <awessel@mail.bradley.edu>
2020-09-16 16:09:48 -0500

    Updated tutorial

---
+++ Spirit Shift/Assets/Tutorial/ButtonPress.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tutorial/ButtonPress")]
+public class ButtonPress : TutorialPart
+{
+    public KeyCode[] buttons;
+    bool[] buttonsPressed;
+
+    // Set up the array of booleans
+    public override void Init()
+    {
+        buttonsPressed = new bool[buttons.Length];
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttonsPressed[i] = false;
+        }
+    }
+
+    // returns true if this part of the tutorial is completed
+    public override bool IsCompleted()
+    {
+        // If any of the buttons has not been pressed, return false
+        for (int i = 0; i < buttonsPressed.Length; i++)
+        {
+            if (!buttonsPressed[i]) return false;
+        }
+
+        return true;
+    }
+    public override void UpdateCompletedTasks()
+    {
+        // Check each button to see if it has been pressed
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (Input.GetKeyDown(buttons[i])) buttonsPressed[i] = true;
+        }
+    }
+}
