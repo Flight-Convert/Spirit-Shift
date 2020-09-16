1127f4c3dcbffa4a2ac46723402fb04109cbabef
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2020-09-16 14:29:08 -0500

    Added basic attack function for the player

---
+++ Spirit Shift/Assets/Scripts/playerAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour
+{
+    public GameObject fist;
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fist.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(punch());
+        }
+    }
+
+    IEnumerator punch()
+    {
+        fist.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        fist.SetActive(false);
+        yield return true;
+    }
+}
