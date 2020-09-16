c7eb7a931fbba3f0bac04c907665e533785c3db1
Anthony <awessel@mail.bradley.edu>
2020-09-15 21:30:52 -0500

    Implemented first piece of tutorial system

---
+++ Spirit Shift/Assets/Tutorial/TutorialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public bool useTutorial;
+
+    public TutorialPart[] tutorialParts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (useTutorial && tutorialParts.Length > 0)
+        {
+            StartCoroutine(waitForTutorialPartCompletion(0));
+        }
+        useTutorial = false;
+    }
+
+    IEnumerator waitForTutorialPartCompletion(int tutorialIndex)
+    {
+        TutorialPart part = tutorialParts[tutorialIndex];
+
+        print(part.prompt);
+
+        while(!part.IsCompleted())
+        {
+            part.UpdateCompletedTasks();
+            yield return null;
+        }
+
+        tutorialIndex++;
+        if (tutorialIndex != tutorialParts.Length)
+            StartCoroutine(waitForTutorialPartCompletion(tutorialIndex));
+        else
+            print("Tutorial Completed!");
+    }
+}
