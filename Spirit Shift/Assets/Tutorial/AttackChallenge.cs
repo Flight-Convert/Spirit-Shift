210ea91df6e78da05ab5b5be883d98cae2b60725
Anthony <awessel@mail.bradley.edu>
2020-09-20 18:32:41 -0500

    Added tutorial challenge for attacking

---
+++ Spirit Shift/Assets/Tutorial/AttackChallenge.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Anthony Wessel
+ * Project 1 (Spirit Shift)
+ * 
+ * A tutorial challenge which requires the player to
+ * attack an enemy
+ */
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tutorial/Attack")]
+public class AttackChallenge : TutorialChallenge
+{
+    bool attacked;
+
+    // Initialize the boolean
+    public override void Init()
+    {
+        attacked = false;
+    }
+
+    // returns true if this part of the tutorial is completed
+    public override bool IsCompleted()
+    {
+        return attacked;
+    }
+
+    public override void UpdateCompletedTasks()
+    {
+        // If player clicked (attack button)
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Find the player's current body
+            BasicMovement body = FindObjectOfType<BasicMovement>();
+
+            // If the player is in an enemies body, then they attacked
+            if (body.GetComponent<attackPlayer>() != null)
+                attacked = true;
+        }
+    }
+}
