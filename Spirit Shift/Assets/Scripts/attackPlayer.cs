405b2602fa8966269bc22001530e625ca4aeca8b
brocEdson <bedson@mail.bradley.edu>
2020-09-20 16:18:51 -0500

    Added charging enemies

--- Spirit Shift/Assets/Scripts/attackPlayer.cs
+++ Spirit Shift/Assets/Scripts/attackPlayer.cs
@@ -3,6 +3,7 @@
  * Spirit Shift
  * Let's the enemy attack the player
  */
+// Uses some code from followPlayer
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +12,16 @@ public class attackPlayer : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private GameObject player;
+    private bool justCharged;
     private float distance;
     public float threshold;
     public float delay;
+    public float chargeDelay;
+    public float chargeForce;
     public GameObject fist;
+
+    // This number specifies the enemy type: 0 for a normal enemy, 1 for a charger, 2 for a shooter
+    public int enemyType = 0;
 
     private void Start()
     {
@@ -34,7 +41,7 @@ public class attackPlayer : MonoBehaviour
             //on left mouse click
             if (Input.GetMouseButtonDown(0))
             {
-                StartCoroutine(punch());
+                StartCoroutine(Attack());
             }
         }
         //if player isn't in enemy body
@@ -43,16 +50,50 @@ public class attackPlayer : MonoBehaviour
             //if enemy is close enough to player body
             if (distance <= threshold)
             {
-                StartCoroutine(punch());
+                StartCoroutine(Attack());
             }
         }
     }
 
-    IEnumerator punch()
+    IEnumerator Attack()
     {
-        fist.SetActive(true);
-        yield return new WaitForSeconds(delay);
-        fist.SetActive(false);
-        yield return true;
+        if(enemyType == 0)
+        {
+            fist.SetActive(true);
+            yield return new WaitForSeconds(delay);
+            fist.SetActive(false);
+            yield return true;
+        }
+        else if(enemyType == 1)
+        {
+            if(!justCharged)
+            {
+                justCharged = true;
+                fist.SetActive(true);
+                rb2d.AddForce(chargeForce * findDirectionFromPos(player.transform.position), ForceMode2D.Impulse);
+                yield return new WaitForSeconds(delay);
+                fist.SetActive(false);
+                yield return new WaitForSeconds(chargeDelay);
+                justCharged = false;
+            }
+            else
+            {
+                yield return true;
+            }
+            
+        }
+        else
+        {
+            yield return true;
+        }
+    }
+
+    Vector2 findDirectionFromPos(Vector2 pos)
+    {
+        Vector2 direction = new Vector2(pos.x - transform.position.x, pos.y - transform.position.y);
+
+        direction = new Vector2(direction.normalized.x, direction.normalized.y);
+
+        return direction;
     }
 }
