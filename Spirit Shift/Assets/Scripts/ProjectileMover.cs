d14dbf231f8c774aac253a3c6ecc6b8ea2c21bde
brocEdson <bedson@mail.bradley.edu>
2020-09-20 20:25:13 -0500

    Added a bullet object for the shooter enemy

---
+++ Spirit Shift/Assets/Scripts/ProjectileMover.cs
@@ -0,0 +1,37 @@
+﻿/* Broc Edson
+ * Spirit Shift
+ * Moves an object forward and then destroys it
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMover : MonoBehaviour
+{
+    public float moveForce;
+    public float speed;
+    public float destroyDelay;
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        StartCoroutine(DestroyAfterDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(rb2d.velocity.magnitude) < speed)
+        {
+            rb2d.AddForce(Vector2.right * moveForce * Time.deltaTime);
+        }
+    }
+
+    IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
+        yield return true;
+    }
+}
