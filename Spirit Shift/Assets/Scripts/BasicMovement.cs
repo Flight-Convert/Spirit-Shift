0d954dac8b91d33a626e7eec89969d6b8d563a62
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2020-10-10 17:00:26 -0500

    Doubled Movement speed when player is possessing a charger

--- Spirit Shift/Assets/Scripts/BasicMovement.cs
+++ Spirit Shift/Assets/Scripts/BasicMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public float speed;
+    private bool isCharger;
     
     //Animation to initialize here
 
@@ -15,6 +17,7 @@ public class BasicMovement : MonoBehaviour
     void Start()
     {
         speed = 10.0f;
+        isCharger = false;
     }
 
     // Update is called once per frame
@@ -23,9 +26,26 @@ public class BasicMovement : MonoBehaviour
         //Basic Movement variable
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
+        //Determines if player is possessing a charger enemy
+        try
+        {
+            isCharger = GetComponent<attackPlayer>().enemyType == 1;
+        }
+        catch (NullReferenceException e)
+        {
+            isCharger = false;
+        }
+
         //animation stuff goes here
 
         //Movement using basic transform
-        transform.position = transform.position + movement * speed * Time.deltaTime;
+        if (isCharger)
+        {
+            transform.position = transform.position + movement * speed * Time.deltaTime * 2;
+        }
+        else
+        {
+            transform.position = transform.position + movement * speed * Time.deltaTime;
+        }
     }
 }
